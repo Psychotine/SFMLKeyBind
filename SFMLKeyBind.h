24329d9004ce475e983b99e121b237f43d202400
johnfoley2048 <C00217207@itcarlow.ie>
2018-03-28 02:56:30 +0100

    Fully changed the key bind class, now containing keyhandler class, changed from ross's one, still more work to be done

--- SFMLKeyBind.h
+++ SFMLKeyBind.h
@@ -2,35 +2,120 @@
 #define SFML_KEY_BIND
 
 #include "SFML\Graphics.hpp"
-
-enum InputType
-{
-	KeyboardInput,
-	MouseInput
-};
-
-struct Key
-{
-	InputType inputType;
-	sf::Event::EventType eventType;
-	sf::Keyboard::Key keyCode;
-	sf::Mouse::Button mouseButton;
-};
+#include <map>
 
 namespace kb
 {
-	static bool inputCheck(const Key t_key, const sf::Event t_e)
+	/// <summary>
+	/// type of input a key can have e.g. the keyboard inputs the event or the mouse does
+	/// </summary>
+	enum InputType
 	{
-		if (t_key.inputType == KeyboardInput && t_key.eventType == t_e.type && t_key.keyCode == t_e.key.code)
+		KeyboardInput,
+		MouseInput
+	};
+
+	/// <summary>
+	/// struct containing all info needed on a jey or mouse button press
+	/// </summary>
+	struct Key
+	{
+		//whether the input is from keyboard or mouse
+		InputType inputType;
+		//the sfml key code
+		sf::Keyboard::Key keyCode;
+		//the sfml mouse button code
+		sf::Mouse::Button mouseButton;
+		//bools to hold wether the button is pressed or released
+		bool isPressed;
+		bool isReleased;
+	};
+
+	/// <summary>
+	/// Key handler class, used to queery sfml wether a particular button has been pressed or released
+	/// </summary>
+	class KeyHandler
+	{
+	public:
+		KeyHandler() = default;
+
+		/// <summary>
+		/// update if a key passed to the function is pressed according to sfml
+		/// </summary>
+		/// <param name="t_key">key struct</param>
+		/// <param name="isPressed">wether the button is pressed</param>
+		void updateKey(sf::Keyboard::Key t_key, bool isPressed)
 		{
-			return true;
+			//assign the bools in the key map(of key structs)
+			m_keyMap[t_key].isPressed = isPressed;
+			m_keyMap[t_key].isReleased = (!isPressed);
 		}
-		if (t_key.inputType == MouseInput && t_key.eventType == t_e.type && t_key.mouseButton == t_e.mouseButton.button)
+
+		/// <summary>
+		/// update if a button passed to the function is pressed according to sfml
+		/// </summary>
+		/// <param name="t_button">key struct</param>
+		/// <param name="isPressed">wether the key is pressed</param>
+		void updateKey(sf::Mouse::Button t_button, bool isPressed)
 		{
-			return true;
+			m_mouseMap[t_button].isPressed = isPressed;
+			m_mouseMap[t_button].isReleased = (!isPressed);
 		}
-		return false;
-	}
+
+		/// <summary>
+		/// Needs to be done after all input has been used as mouse button released causes problems looking into it but for now problem is hacked fixed
+		/// </summary>
+		void reset()
+		{
+			for (auto & x : m_keyMap)
+			{
+				x.second.isReleased = false;
+			}
+			for (auto & x : m_mouseMap)
+			{ 
+				x.second.isReleased = false;
+			}
+		}
+
+		/// <summary>
+		/// returns wether the button or key in the key map is pressed
+		/// </summary>
+		/// <param name="t_key">the key/button being queeried</param>
+		/// <returns>boolean</returns>
+		bool isPressed(const Key t_key) 
+		{
+			if (t_key.inputType == KeyboardInput)
+			{
+				return m_keyMap[t_key.keyCode].isPressed;
+			}
+			else if (t_key.inputType == MouseInput)
+			{
+				return m_mouseMap[t_key.mouseButton].isPressed;
+			}
+		}
+		/// <summary>
+		/// returns wether the button or key in the key map is released
+		/// </summary>
+		/// <param name="t_key">the key/button being queeried</param>
+		/// <returns>boolean</returns>
+		bool isReleased(const Key t_key)
+		{
+			if (t_key.inputType == KeyboardInput)
+			{
+				return m_keyMap[t_key.keyCode].isReleased;
+			}
+			else if (t_key.inputType == MouseInput)
+			{
+				return m_mouseMap[t_key.mouseButton].isReleased;
+			}
+		}
+	private:
+		typedef std::map<sf::Keyboard::Key, Key> KeyMap;
+		typedef std::map<sf::Mouse::Button, Key> MouseMap;
+
+		KeyMap m_keyMap;
+		MouseMap m_mouseMap;
+	};
 }
 
 #endif
